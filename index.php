8793d50e2dd2f7c92c20e7ea9129bf6245f6b0c6
IreneChicca <irene.chicca@outlook.com>
2023-09-28 16:36:59 +0200

    Print discs

--- index.php
+++ index.php
@@ -5,16 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="./main.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" />
     <title>Document</title>
 </head>
 
 <body>
     <div id="app">
 
+        <header class=""></header>
+        <main class="pt-5">
+
+            <div class="card-box container p-5 d-flex justify-content-evenly flex-wrap">
+
+                <div class="card col-3 p-3 m-2" v-for="disc in discs">
+                    <img :src=disc.poster alt="">
+                    <h5>{{ disc.title }}</h5>
+                    <p>{{ disc.author }}</p>
+                    <h6>{{ disc.year }}</h6>
+
+                </div>
 
 
+            </div>
+
+        </main>
     </div>
 </body>
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js"
+    integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </html>
--- main.js
+++ main.js
@@ -3,7 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      discs: [],
     };
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost/PHP/php-dischi-json/discInfo.php")
+      .then((res) => {
+        console.log(res.data);
+
+        // const discInfo = ;
+        this.discs = res.data;
+      });
   },
 }).mount("#app");
